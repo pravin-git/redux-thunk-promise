2c272342e2cd1c8a24dab40aa677bbb4de7459c7
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2018-08-09 10:26:51 -0400

    refactoring

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Boilerplate for React-Redux-Logger-Thunk-Promise
+Presentational and container pattern.
--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -3,25 +3,26 @@ import { connect } from 'react-redux';
 import { fetchUsers, fetchUsersPromise  } from '../actions/userActions'
 
 function mapStateToProps(state, props){
-    return{
+    return {
       //user is name defined in combinedreducer
       users : state.user.users,
       showHeader : state.user.fetched
     };
-  }
+}
   
   const mapDispatchToProps = (dispatch) =>{
     return {
-      fetchUsers: () =>{
+      fetchUsers: () => {
         dispatch({type:"Fetch_User_start"});
         //Thunkway
         //dispatch(fetchUsers());
         //Promiseway
         dispatch(fetchUsersPromise()).then((resp)=> {
-          console.log('asdfsdf', resp);
+            //resolve promise here
+            console.log('asdfsdf', resp);
         });
       }
-    }
-  }
+   }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserComponent);
