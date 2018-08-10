d00d4607a7f1248dfd19101cc73d7f0ee3d469e0
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2018-08-10 16:39:18 -0400

    formatting

--- src/App.test.js
+++ src/App.test.js
@@ -5,18 +5,23 @@ import App from './App';
 import userReducer  from './reducers/userReducer';
 
 it('userReducer', () =>{
+
   expect(userReducer(undefined, {})).toEqual({"error": null, "fetched": false, "fetching": false, "users": []});
+  
   const action = { type : 'Fetch_User_start'} ;
   expect(userReducer(undefined, action)).toEqual({"error": null, "fetched": false, "fetching": true, "users": []});
+  
   const actionPending = { type : 'User_PENDING'} ;
   expect(userReducer(undefined, actionPending)).toEqual({"error": null, "fetched": false, "fetching": true, "users": []});
+  
   const actionUserReceived = { type : 'Fetch_users_received', payload : []} ;
   expect(userReducer(undefined, actionUserReceived)).toEqual({"error": null, "fetched": true, "fetching": false, "users": actionUserReceived.payload});
+  
   const actionUserFulfilled = { type : 'User_FULFILLED', payload : []} ;
   expect(userReducer(undefined, actionUserFulfilled)).toEqual({"error": null, "fetched": true, "fetching": false, "users": actionUserFulfilled.payload.data});
+  
   const actionError = { type : 'Fetch_users_error', payload : []} ;
   expect(userReducer(undefined, actionError)).toEqual({"error": actionError.payload, "fetched": false, "fetching": false, "users": []});
-  
 
 });
 
